8e3478132ab3594bb21a8245e0db7c8f31387b2b
grace-morganmaxwell <gracemorganmaxwell@gmail.com>
2023-08-08 09:44:32 +1200

    Add files via upload
    Works locally when tested using Visual Studio 2022 and codded with C# v7.0.

---
+++ CelsiusToFahrenheitConvertor/CelsiusToFahrenheitConvertor/TemperatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    public static class Converter
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the Celsius to Fahrenheit Converter 3000");
+            Console.WriteLine("I can convert Celsius to Fahrenheit for you. Please enter the temperature in Celsius:");
+
+            if (double.TryParse(Console.ReadLine(), out double celsiusTemperature) && celsiusTemperature >= -271.15)
+            {
+                double fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+                Console.WriteLine($"The temperature in Fahrenheit is: {fahrenheitTemperature:F2}°F");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input or temperature below absolute zero (-271.15°C). Please enter a valid temperature.");
+            }
+        }
+    }
+}
